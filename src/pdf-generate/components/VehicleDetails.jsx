0dc2dfe0441c18ad6e014ed68049f97463569b6d
Danesh <techydanesh@gmail.com>
2026-09-22 15:02:47 +0530

    fix: job card details table

--- src/pdf-generate/components/VehicleDetails.jsx
+++ src/pdf-generate/components/VehicleDetails.jsx
@@ -8,6 +8,8 @@ const sectionHeaderClass =
 const cellClass =
   "border border-black px-[4px] py-[2px] text-[13px] leading-tight align-top";
 
+const labelClass = `${cellClass} font-bold whitespace-nowrap`;
+
   return (
     <table className="w-full border-collapse mt-1 mb-0 text-[13px] font-sans">
       <tbody>
@@ -21,28 +23,28 @@ const cellClass =
         </tr>
 
         <tr>
-          <td className={`${cellClass} font-bold w-[20%]`}>
+          <td className={`${labelClass} w-[16%]`}>
             VEHICLE MODEL
           </td>
-          <td className={`${cellClass} w-[30%]`}>
+          <td className={`${cellClass} w-[34%]`}>
             {invoice.vehicle?.vehicle_model}
           </td>
-          <td className={`${cellClass} font-bold w-[20%]`}>
+          <td className={`${labelClass} w-[23%]`}>
             CREATION DATE
           </td>
-          <td className={`${cellClass} w-[30%]`}>
+          <td className={`${cellClass} w-[27%]`}>
             {fmtDate(invoice.created_at)}
           </td>
         </tr>
 
         <tr>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             REGISTRATION
           </td>
           <td className={cellClass}>
             {invoice.vehicle?.registration_number}
           </td>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             REPAIR TYPE
           </td>
           <td className={cellClass}>
@@ -51,13 +53,13 @@ const cellClass =
         </tr>
 
         <tr>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             CHASSIS NO
           </td>
           <td className={cellClass}>
             {invoice.vehicle?.chassis_no}
           </td>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             KM/HM READING
           </td>
           <td className={cellClass}>
@@ -66,13 +68,13 @@ const cellClass =
         </tr>
 
         <tr>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             AGGREGATE NO
           </td>
           <td className={cellClass}>
             {invoice.vehicle?.aggregate_no}
           </td>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             Cum. KM/HM READING
           </td>
           <td className={cellClass}>
@@ -81,14 +83,14 @@ const cellClass =
         </tr>
 
         <tr>
-          <td className={`${cellClass} font-bold`}>
+          <td className={labelClass}>
             ENGINE NO
           </td>
           <td className={cellClass}>
             {invoice.vehicle?.engine_no}
           </td>
 
-          <td colSpan={2} className="border-b border-r border-black py-0 px-[4px] align-top"></td>
+          <td colSpan={2} className="border-none py-0 px-[4px] align-top"></td>
         </tr>
       </tbody>
     </table>
